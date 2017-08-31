b2c577a7af22937948abc6354e8c029b1fc0e9bf
Alex Perez <alexperez.dev@gmail.com>
2017-09-01 00:16:04 +1200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { GridList, GridTile } from 'material-ui/GridList'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+const columns = [
+  {
+    id: 1,
+    name: 'ToDo',
+  }, {
+    id: 2,
+    name: 'In Progress',
+  }, {
+    id: 3,
+    name: 'Done',
+  },
+];
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  tileChild: {
+    width: 50,
+    height: 50,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: 'auto',
+  },
+};
+
+const App = () => (
+  <MuiThemeProvider>
+    <div>
+      <h1>skrambun</h1>
+      <div style={styles.root}>
+        <GridList style={styles.gridList} cellHeight={100} cols={columns.length}>
+          {columns.map(column => (
+            <GridTile key={column.id} title={column.name}>
+              <div style={styles.tileChild}></div>
+            </GridTile>
+          ))}
+        </GridList>
       </div>
-    );
-  }
-}
+    </div>
+  </MuiThemeProvider>
+);
 
 export default App;
