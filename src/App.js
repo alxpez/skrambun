a3997732be63c6a3dcda0a0bb710f37b945848f9
Alex Perez <alxpez@AMB.local>
2017-09-02 19:27:38 +1200

    refactor layout, update material-ui

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
 import React from 'react';
-import { GridList, GridTile } from 'material-ui/GridList'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import AddIcon from 'material-ui-icons/Add'
+import { GridList, GridListTile, GridListTileBar } from 'material-ui/GridList';
+import { MockData } from './mocks'
 import './App.css';
 
 
-const columns = [
-  {
-    id: 1,
-    name: 'ToDo',
-  }, {
-    id: 2,
-    name: 'In Progress',
-  }, {
-    id: 3,
-    name: 'Done',
-  },
-];
-
 const App = () => (
-  <MuiThemeProvider>
-    <div>
+  <div className="root">
+    <AppBar position="static" color="default">
+      <Toolbar disableGutters>
+        <IconButton className="menuButton" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography type="title" className="flex">
+          skrambun
+        </Typography>
+      </Toolbar>
+    </AppBar>
 
-      <h1>skrambun</h1>
-      <div>Backlog</div>
-      <div>
-        <GridList cellHeight={100} cols={columns.length}>
-          {columns.map(column => (
-            <GridTile key={column.id} title={column.name}>
-              <div></div>
-            </GridTile>
-          ))}
-        </GridList>
-      </div>
-    </div>
-  </MuiThemeProvider>
+    <Button raised className="backlogButton">Backlog</Button>
+
+    <GridList className="gridList" cellHeight={75} cols={MockData.columns.length}>
+      {MockData.columns.map(column => (
+
+          <GridListTile key={column.id}>
+            <GridListTileBar
+              title={column.name}
+              subtitle={<span>stories: {column.stories.length}</span>}
+              actionIcon={
+                <IconButton>
+                  <AddIcon color="rgba(255, 255, 255, 0.54)" />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+      ))}
+    </GridList>
+  </div>
 );
 
 export default App;
---
+++ src/mocks.js
@@ -0,0 +1,95 @@
+export const MockData = {
+  columns: [
+    {
+      id: 1,
+      name: 'ToDo',
+      stories: [
+        {
+          id: 1,
+          title: 'Un todo',
+          description: 'Pues eso te cuento',
+          reportedBy: 'Perry Potter',
+          assignedTo: 'Chema Del Tema',
+        },{
+          id: 2,
+          title: 'Otro todo',
+          description: 'Te cuento lo mismo',
+          reportedBy: 'Perry Potter',
+          assignedTo: '',
+        },{
+          id: 3,
+          title: 'Un todo mas',
+          description: 'Pues eso te cuento',
+          reportedBy: 'Chema Del Tema',
+          assignedTo: '',
+        },{
+          id: 4,
+          title: 'El todo',
+          description: 'El todo por el todo',
+          reportedBy: 'Ramon Ramirez',
+          assignedTo: 'Chema Del Tema',
+        },
+      ],
+    }, {
+      id: 2,
+      name: 'In Progress',
+      stories: [
+        {
+          id: 1,
+          title: 'Algo que esta pasando',
+          description: 'Pues eso te cuento',
+          reportedBy: 'Perry Potter',
+          assignedTo: 'Chema Del Tema',
+        },{
+          id: 2,
+          title: 'Otra cosa mariposa',
+          description: 'Te cuento lo mismo',
+          reportedBy: 'Perry Potter',
+          assignedTo: '',
+        },
+      ],
+    }, {
+      id: 3,
+      name: 'Done',
+      stories: [
+        {
+          id: 1,
+          title: 'Hecho',
+          description: 'Pues eso te cuento',
+          reportedBy: 'Perry Potter',
+          assignedTo: 'Chema Del Tema',
+        },{
+          id: 2,
+          title: 'Otro todo hecho',
+          description: 'Te cuento lo mismo',
+          reportedBy: 'Perry Potter',
+          assignedTo: '',
+        },{
+          id: 3,
+          title: 'Uno mas hecho',
+          description: 'Pues eso te cuento',
+          reportedBy: 'Chema Del Tema',
+          assignedTo: '',
+        },{
+          id: 4,
+          title: 'El todo hecho',
+          description: 'El todo por el todo',
+          reportedBy: 'Ramon Ramirez',
+          assignedTo: 'Chema Del Tema',
+        },{
+          id: 5,
+          title: 'La nada finalizada',
+          description: 'El todo por el todo',
+          reportedBy: 'Ramon Ramirez',
+          assignedTo: 'Chema Del Tema',
+        },{
+          id: 6,
+          title: 'El perro del hortelano',
+          description: 'El todo por el todo',
+          reportedBy: 'Ramon Ramirez',
+          assignedTo: 'Chema Del Tema',
+        },
+      ],
+    },
+  ],
+};
